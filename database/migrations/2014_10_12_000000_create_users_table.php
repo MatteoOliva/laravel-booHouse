1a5f99b712c7697460104f85807d6ec199964115
MatteoOliva <oliva.matteo98@outlook.com>
2024-05-02 15:34:53 +0200

    feat: created migration e model apartment

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('name', 100);
+            $table->string('surname', 100);
+            $table->date('birth_date');
+            $table->string('email', 100)->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('password', 30);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2024_05_02_130316_create_apartments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            
+            $table->string('title', 100);
+            $table->string('slug', 100)->unique();
+            $table->text('description');
+            $table->tinyInteger('rooms')->unsigned();
+            $table->tinyInteger('beds')->unsigned();
+            $table->tinyInteger('toilets')->unsigned();
+            $table->float('mq', 7,3);
+            $table->string('image');
+            $table->string('lat');
+            $table->string('lon');
+            $table->string('address');
+            $table->boolean('visible');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
