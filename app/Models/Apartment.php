a4d2db007c0da106841928f873246ea10c572a4c
MatteoOliva <oliva.matteo98@outlook.com>
2024-05-02 15:50:09 +0200

    feat: created  services table

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -11,4 +11,7 @@ class Apartment extends Model
     public function user() {
         return $this->belongsTo(User::class);
       }
+      public function services() {
+        return $this->belongsToMany(Service::class);
+      }
 }
--- database/migrations/2024_05_02_130316_create_apartments_table.php
+++ database/migrations/2024_05_02_130316_create_apartments_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('apartments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
-            
+
             $table->string('title', 100);
             $table->string('slug', 100)->unique();
             $table->text('description');
