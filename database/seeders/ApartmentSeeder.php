b168c3dd02355f6f8d8e06009eeaadb1248369f9
SerenaMalusa <sere.malu@gmail.com>
2024-05-03 11:49:43 +0200

    fix: add a comment to apartment seeder

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -81,6 +81,7 @@ class ApartmentSeeder extends Seeder
                     }
                     // add the slug to the array and in the new apartment
                     array_push($slugs, $new_slug);
+                    // save the apartment in the db
                     $apartment->slug = $new_slug;
 
                     // save the apartment in the db
