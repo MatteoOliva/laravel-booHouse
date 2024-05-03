8b5d78699fb054549279c6bdc7c570f9c60639db
lorenzodisanto <lore.disanto95@gmail.com>
2024-05-03 16:55:52 +0200

    feat: add img

--- resources/views/auth/apartments/index.blade.php
+++ resources/views/auth/apartments/index.blade.php
@@ -8,18 +8,23 @@
         <div class="row g-3">
             @foreach ($apartments as $apartment)
             <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">{{ $apartment->title }}</h5>
-                      <a href="#" class="btn btn-primary">Dettaglio</a>
-                      <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" @if ($apartment->visible) checked                           
-                        @endif>
-                        <label class="form-check-label" for="flexSwitchCheckChecked">Visibile</label>
+                <a href="">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ $apartment->image }}" class="img-fluid" alt="#">
+                        <div class="card-body">
+                          <h5 class="card-title">{{ $apartment->title }}</h5>
+                          <a href="#" class="btn btn-primary"><i class="fa-solid fa-pen"></i></a>
+                          <a href="#" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
+
+                          <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" @if ($apartment->visible) checked                           
+                            @endif>
+                            <label class="form-check-label" for="flexSwitchCheckChecked">Visibile</label>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
+                </a>
+                
             </div>
             @endforeach
         </div>
