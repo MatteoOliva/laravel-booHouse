4f1abb08def59996544740939f8df74e269f373f
MicheleIanni86 <micheleianni86@gmail.com>
2024-05-06 23:48:51 +0200

    feat: Adjusted Style of Home, Header and index

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-dark text-white">
+<footer class="bg-dark text-white" style="background-color: #0A0F15 !important">
   <div class="container py-4">
     &copy; {{ env('APP_OWNER') }} {{ now()->year }}
   </div>
