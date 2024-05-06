da2316b44781b90ca06e0a31f7d02d91211ef46b
SerenaMalusa <sere.malu@gmail.com>
2024-05-06 11:52:24 +0200

    feat: add create unique slug function to apartment model

--- app/Http/Controllers/Auth/ApartmentController.php
+++ app/Http/Controllers/Auth/ApartmentController.php
@@ -55,45 +55,12 @@ class ApartmentController extends Controller
         // set the visibility to false
         $apartment->visible = false;
 
-        // get all the slugs from the db
-        $existing_slugs = Apartment::all()->pluck('slug')->toArray();
-        // dd($existing_slugs);
-
-        // create a slug from the title
-        $new_slug = Str::slug($apartment->title);
-        // check if this new slug already exist in the db
-        while (in_array($new_slug, $existing_slugs)) {
-
-            // get only the last 2 carachters of the slug then parse them into int
-            $last_digits = substr($new_slug, strlen($new_slug) - 2);
-            $last_num = (int) $last_digits;
-
-            // if this is a number 
-            if ($last_num != 0) {
-
-                // add 1 
-                $new_num = $last_num + 1;
-                // then if the num is < 10 add a 0 before it
-                if ($new_num < 10) $new_num = '0' . $new_num;
-                // get all the slug except the last 2 characters
-                $slug_text = substr($new_slug, 0, -3);
-            } else {
-
-                // the new num to add will be 01
-                $new_num = '01';
-                // the slug text will be = to the whole slug
-                $slug_text = $new_slug;
-            }
-
-            // then add the number to the text of the slug
-            $new_slug = $slug_text . '-' . $new_num;
-        }
-
         // add the slug to the apartment and save the apartment in the db
-        $apartment->slug = $new_slug;
+        $apartment->slug = $apartment->create_unique_slug();
         $apartment->save();
 
         return redirect()->route('user.apartments.show', $apartment);
+        // dd($apartment);
     }
 
     /**
--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
 
 class Apartment extends Model
 {
@@ -24,6 +26,46 @@ class Apartment extends Model
     'address',
     'visible'
   ];
+
+  public function create_unique_slug()
+  {
+    // get all the slugs from the db
+    $existing_slugs = Apartment::all()->pluck('slug')->toArray();
+    // dd($existing_slugs);
+
+    // create a slug from the title
+    $new_slug = Str::slug($this->title);
+
+    // check if this new slug already exist in the db
+    while (in_array($new_slug, $existing_slugs)) {
+
+      // get only the last 2 carachters of the slug then parse them into int
+      $last_digits = substr($new_slug, strlen($new_slug) - 2);
+      $last_num = (int) $last_digits;
+
+      // if this is a number 
+      if ($last_num != 0) {
+
+        // add 1 
+        $new_num = $last_num + 1;
+        // then if the num is < 10 add a 0 before it
+        if ($new_num < 10) $new_num = '0' . $new_num;
+        // get all the slug except the last 2 characters
+        $slug_text = substr($new_slug, 0, -3);
+      } else {
+
+        // the new num to add will be 01
+        $new_num = '01';
+        // the slug text will be = to the whole slug
+        $slug_text = $new_slug;
+      }
+
+      // then add the number to the text of the slug
+      $new_slug = $slug_text . '-' . $new_num;
+    }
+
+    return $new_slug;
+  }
 
   public function user()
   {
