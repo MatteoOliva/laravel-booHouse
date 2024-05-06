b5ba9747112d6f40b9abd356fbf455b7dbd2963e
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-05-06 12:14:36 +0200

    feat: Style Show

--- resources/views/auth/apartments/index.blade.php
+++ resources/views/auth/apartments/index.blade.php
@@ -22,13 +22,13 @@
                         <div class="card-body">
                           <h5 class="card-title text-black">{{ $apartment->title }}</h5>
                             <div class="d-md-flex justify-content-md-end">
-                             <a href="#" class="btn btn-primary mx-1" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .70rem;"><i class="fa-solid fa-pen"></i></a>
-                             <a href="#" class="btn btn-danger mx-1" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .70rem;"><i class="fa-solid fa-trash"></i></a>
+                             <a href="#" class="btn text-white fw-semibold mx-1" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .70rem; background-color: #1278c6"><i class="fa-solid fa-pen"></i></a>
+                             <a href="#" class="btn text-white fw-semibold mx-1" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .70rem; background-color: #A33B3B"><i class="fa-solid fa-trash"></i></a>
                             </div>
-                             <div class="form-check form-switch">
-                               <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" @if ($apartment->visible) checked                           
-                               @endif
-                               <label class="form-check-label" for="flexSwitchCheckChecked">Visibile</label>
+                             <div class="form-check form-switch fs-5">
+                               <input class="form-check-input" style="background-color: #1278c6;" type="checkbox" role="switch" id="flexSwitchCheckChecked" @if ($apartment->visible) checked                           
+                               @endif>
+                               <label class="form-check-label fw-semibold" for="flexSwitchCheckChecked">Visibile</label>
                           </div>
                         </div>
                       </div>
