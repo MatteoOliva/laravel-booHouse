c908ffaf2dfc87f432709c2c5fe2a0bce5a55470
lorenzodisanto <lore.disanto95@gmail.com>
2024-05-06 22:54:20 +0200

    style: form row

--- resources/views/auth/apartments/form.blade.php
+++ resources/views/auth/apartments/form.blade.php
@@ -17,79 +17,91 @@
             @endif
 
             <div class="row">
-    
-                <div class="col-6">
-                    <label for="description" class="form-label">Titolo</label>
-                    <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ?? $apartment->title ?? '' }}"/>
-                </div>
-
-                <div class="col-6">
-                    <label for="description" class="form-label">Descrizione</label>
-                    <textarea class="form-control" id="description" name="description" cols="30" rows="10">{{ old('description') ?? $apartment->description ?? ''  }}</textarea>
-                </div>
-
-                <div class="col-6">
-                    <label for="rooms" class="form-label">N. di camere</label>
-                    <input type="number" class="form-control" id="rooms" name="rooms" min="1" value="{{ old('rooms') ?? $apartment->rooms ?? '' }}"/>
-                </div>
-
-                <div class="col-6">
-                    <label for="beds" class="form-label">N. di letti</label>
-                    <input type="number" class="form-control" id="beds" name="beds" value="{{ old('beds') ?? $apartment->beds ?? '' }}"/>
-                </div>
-
-                <div class="col-6">
-                    <label for="toilets" class="form-label">N. di bagni</label>
-                    <input type="number" class="form-control" id="toilets" name="toilets" min="1" value="{{ old('toilets') ?? $apartment->toilets ?? '' }}"/>
-                </div>
-
-                <div class="col-6">
-                    <label for="mq" class="form-label">Metri quadri</label>
-                    <input type="number" class="form-control" id="mq" name="mq" min="5" value="{{ old('mq') ?? $apartment->mq ?? '' }}"/>
-                </div>
-
-                <div class="col-6">                
-                    @if(isset($apartment->image))
-                        <div class="form-label">Immagine</div>
-                        <img class="img-fluid mb-3" src="{{ asset('storage/' . $apartment->image) }}" alt="apartment image">
-                        <label for="image" class="form-label">Cambia immagine</label>
-                    @else
-                        <label for="image" class="form-label">Aggiungi Immagine</label>
-                    @endif
-                    <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" value="{{ old('image') ?? '' }}">
-                    @error('image')
-                        <div class="invalid-feedback">
-                            {{ $message }}
+                <div class="col-9">
+                    <div class="row">
+                        <div class="col-12">
+                            <label for="description" class="form-label mt-3">Titolo</label>
+                            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ?? $apartment->title ?? '' }}"/>
                         </div>
-                    @enderror
-                </div>
-
-                @if(isset($apartment->image))
-                    
-                    <div class="col-6 d-flex flex-column justify-content-end align-items-start">
-                        <div class="btn btn-danger" id="delete-img-btn">Delete Image</div>
+        
+                        <div class="col-12">
+                            <label for="description" class="form-label mt-3">Descrizione</label>
+                            <textarea class="form-control" id="description" name="description" cols="30" rows="10">{{ old('description') ?? $apartment->description ?? ''  }}</textarea>
+                        </div>
+        
+                        <div class="col-12">
+                            <label for="rooms" class="form-label mt-3">N. di camere</label>
+                            <input type="number" class="form-control" id="rooms" name="rooms" min="1" value="{{ old('rooms') ?? $apartment->rooms ?? '' }}"/>
+                        </div>
+        
+                        <div class="col-12">
+                            <label for="beds" class="form-label mt-3">N. di letti</label>
+                            <input type="number" class="form-control" id="beds" name="beds" value="{{ old('beds') ?? $apartment->beds ?? '' }}"/>
+                        </div>
+        
+                        <div class="col-12">
+                            <label for="toilets" class="form-label mt-3">N. di bagni</label>
+                            <input type="number" class="form-control" id="toilets" name="toilets" min="1" value="{{ old('toilets') ?? $apartment->toilets ?? '' }}"/>
+                        </div>
+        
+                        <div class="col-12">
+                            <label for="mq" class="form-label mt-3">Metri quadri</label>
+                            <input type="number" class="form-control" id="mq" name="mq" min="5" value="{{ old('mq') ?? $apartment->mq ?? '' }}"/>
+                        </div>
+        
+                        <div class="col-12 ">                
+                            @if(isset($apartment->image))
+                                <div class="form-label mt-3">Immagine</div>
+                                <div class="image-cover">
+                                    <img class="img-fluid mb-3 " src="{{ asset('storage/' . $apartment->image) }}" alt="apartment image">
+                                    <div class="col-6 d-flex flex-column justify-content-end align-items-start">
+                                        <div class="btn btn-danger mb-3 image-trash" id="delete-img-btn"><i class="fa-solid fa-x"></i></div>
+                                    </div>
+                                </div>                               
+                                {{-- <label for="image" class="form-label">Cambia immagine</label> --}}
+                            @else
+                                <label for="image" class="form-label mt-3">Aggiungi Immagine</label>
+                            @endif
+                            @if(isset($apartment->image))
+                            
+                            
+                            
+                        @endif
+                            <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" value="{{ old('image') ?? '' }}">
+                            @error('image')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+        
+                        
+        
+        
+                        <div class="col-12">
+                            <label for="address" class="form-label mt-3">Indirizzo</label>
+                            <input type="text" class="form-control" id="address" name="address" value="{{ old('address') ?? $apartment->address ?? '' }}"/>
+                        </div>
                     </div>
-                    
-                @endif
-
-
-                <div class="col-6">
-                    <label for="address" class="form-label">Indirizzo</label>
-                    <input type="text" class="form-control" id="address" name="address" value="{{ old('address') ?? $apartment->address ?? '' }}"/>
                 </div>
+                <div class="col-3">                  
+                            <div class="form-label mt-3">Servizi aggiuntivi disponibili</div>
+                            <div >
+                                @foreach ($services as $service)
+                                    <div class="my-2">
+                                        <input type="checkbox" name="services[]" id="tags-{{ $service->id }}" value="{{ $service->id }}" {{ in_array($service->id, old('service') ?? $related_services_ids ?? []) ? 'checked' : '' }} class="me-1">
+                                        <img src="{{ '/' . $service->icon }}" alt="" style="width: 30px" class="me-1">
+                                        <label for="tags-{{ $service->id }}" class="me-1">{{ $service->name }}</label>
+                                        
+                                    </div>
+                                @endforeach                    
+                    
+                </div>
+            </div>
+
+            
                 
-                <div class="col">
-                    <div class="form-label mb-1">Servizi aggiuntivi disponibili</div>
-                    <div class="d-flex flex-wrap">
-                        @foreach ($services as $service)
-                            <div>
-                                <input type="checkbox" name="services[]" id="tags-{{ $service->id }}" value="{{ $service->id }}" {{ in_array($service->id, old('service') ?? $related_services_ids ?? []) ? 'checked' : '' }} class="ms-1">
-                                <label for="tags-{{ $service->id }}" class="ms-3">{{ $service->name }}</label>
-                                <img src="{{ '/' . $service->icon }}" alt="">
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
+                
 
                 {{-- hidden fields for lat and lon --}}
                 <input type="hidden" id="lat" name="lat" value="">
@@ -97,9 +109,7 @@
     
             </div>
 
-            <button type="submit" class="btn btn-primary" id="save-button-form">Save</button>
-            
-            <button type="submit" class="btn btn-danger" id="save-button-form">Save</button>
+            <button type="submit" class="btn btn-primary mt-3" id="save-button-form">Salva</button>
         </form>
 
         @if(isset($apartment->image))
