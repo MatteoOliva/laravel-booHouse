38e21538c9cf05417d877f0a039ce22e26220e75
lorenzodisanto <lore.disanto95@gmail.com>
2024-05-08 11:57:12 +0200

    feat: add View and Message seeder in Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,7 +28,9 @@ class DatabaseSeeder extends Seeder
             ApartmentSeeder::class,
             SponsorshipSeeder::class,
             ServiceSeeder::class,
-            ApartmentServiceSeeder::class
+            ApartmentServiceSeeder::class,
+            MessageSeeder::class,
+            ViewSeeder::class
         ]);
     }
 }
