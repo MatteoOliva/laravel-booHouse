9eb616dac1ae0f5ded602061756d04aefec895fb
SerenaMalusa <sere.malu@gmail.com>
2024-05-08 16:24:32 +0200

    feat: autorizzare cors solo da backend e forntend

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::apiResource('Apartments', ApiController::class)->only(['index', 'show']);
-Route::get('/search/{search_param}', [ApiController::class, 'search'])->name('api.search');
+Route::get('apartments/search/{search_param}', [ApiController::class, 'search'])->name('api.apartments.search');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
