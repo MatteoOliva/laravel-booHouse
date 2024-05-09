388cbf435190de9fa7a13943bc23c4876cb674b6
SerenaMalusa <98231830+SerenaMalusa@users.noreply.github.com>
2024-05-09 16:03:38 +0200

    aggiunta rotta per api index

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::apiResource('Apartments', ApiController::class)->only(['index', 'show']);
+// Route::apiResource('Apartments', ApiController::class)->only(['index']);
+Route::get('apartments', [ApiController::class, 'index'])->name('api.apartments.index'); // restituisce sempre tutti gli appartamenti
 Route::get('apartments/{slug}', [ApiController::class, 'show'])->name('api.apartments.show'); // riceve slug e mostra appartamento
 Route::get('apartments/search/{search_param}', [ApiController::class, 'search'])->name('api.apartments.search'); // riceve termine di ricerca e restituisce elenco di appartanmenti
 Route::get('apartments/search/sponsored/{search_term}', [ApiController::class, 'sponsored_search'])->name(('api.apartments.search.sponsored')); // riceve termine di ricerca e restituisce elenco di appartamenti sponsorizzati
+Route::get('apartments/search/not_sponsored/{search_term}', [ApiController::class, 'not_sponsored_search'])->name('api.apartments.searc.not_sponsored');
 Route::get('apartments/sponsored/all', [ApiController::class, 'sponsored_all'])->name('api.apartments.sponsored.all'); // riceve niente e mostra tutti gli appartamenti sponsorizzati
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
