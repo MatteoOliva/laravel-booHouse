645007c37c13f67e492f8cf5dc10172b711396a9
SerenaMalusa <sere.malu@gmail.com>
2024-05-09 19:48:04 +0200

    feat: ricerca per raggio con where or che cerca nel titolo

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('apartments', [ApiController::class, 'index'])->name('api.apartments.index'); // restituisce sempre tutti gli appartamenti
 Route::get('apartments/{slug}', [ApiController::class, 'show'])->name('api.apartments.show'); // riceve slug e mostra appartamento
 Route::get('apartments/search/{search_param}', [ApiController::class, 'search'])->name('api.apartments.search'); // riceve termine di ricerca e restituisce elenco di appartanmenti
-Route::get('apartments/search/ordered/{lat}/{lon}/{radius}', [ApiController::class, 'search_ordered'])->name('api.apartments.search.ordered');
-Route::get('apartments/search/not_sponsored/{search_term}', [ApiController::class, 'not_sponsored_search'])->name('api.apartments.searc.not_sponsored'); // riceve termine di ricerca e restituisce appartamenti non sponsorizzati
+Route::get('apartments/search/ordered/{search_term}/{lat}/{lon}/{radius}', [ApiController::class, 'search_ordered'])->name('api.apartments.search.ordered');
+Route::get('apartments/search/not_sponsored/{search_term}', [ApiController::class, 'not_sponsored_search'])->name('api.apartments.search.not_sponsored'); // riceve termine di ricerca e restituisce appartamenti non sponsorizzati
 Route::get('apartments/search/sponsored/{search_term}', [ApiController::class, 'sponsored_search'])->name(('api.apartments.search.sponsored')); // riceve termine di ricerca e restituisce elenco di appartamenti sponsorizzati
 Route::get('apartments/sponsored/all', [ApiController::class, 'sponsored_all'])->name('api.apartments.sponsored.all'); // riceve niente e mostra tutti gli appartamenti sponsorizzati
 
