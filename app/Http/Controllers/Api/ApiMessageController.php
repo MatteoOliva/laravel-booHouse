c09dbbd750fd83b35c2dc618001cf3a8d24b3936
lorenzodisanto <lore.disanto95@gmail.com>
2024-05-12 10:42:51 +0200

    feat: add store method to save new message in db

--- app/Http/Controllers/Api/ApiMessageController.php
+++ app/Http/Controllers/Api/ApiMessageController.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class ApiMessageController extends Controller
 {
-    public function store(Request $request){
+
+     /**
+     * Display a listing of the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+
         
     }
+
+
+     /**
+     * Display a listing of the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request){
+        
+        $data = $request->all();
+
+        // Salvataggio del messaggio nel database
+        $message = new Message();
+        $message->apartment_id = $data['apartment_id'];
+        $message->email = $data['email'];
+        $message->content = $data['content'];
+       
+        $message->save();
+
+        // risposta
+        return response()->json(['response' => true, 'message' => 'messaggio inviato con successo']);
+    }
+
+
 }
